739c2d3b94622dbc87ae09f129d61e0809a08576
pattabhia <shivaram2004@gmail.com>
2020-04-20 08:35:33 +0530

    Adding Zuul Example
    Adding Zuul Example

---
+++ microservice-projects/zuul-example/src/main/java/com/abhiram/zuul/example/ZuulExampleApplication.java
@@ -0,0 +1,13 @@
+package com.abhiram.zuul.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulExampleApplication.class, args);
+    }
+
+}
---
+++ microservice-projects/zuul-example/src/test/java/com/abhiram/zuul/example/ZuulExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiram.zuul.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

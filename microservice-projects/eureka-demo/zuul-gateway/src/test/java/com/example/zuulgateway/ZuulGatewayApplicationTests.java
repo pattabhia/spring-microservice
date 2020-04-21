bbfe4a4b5a810e0b99ef78fe7f4297b729ed5e3f
pattabhia <shivaram2004@gmail.com>
2020-04-21 16:21:29 +0530

    adding zuul gateway example

--- microservice-projects/eureka-demo/zuul-gateway/src/test/java/com/example/zuulgateway/ZuulGatewayApplicationTests.java
+++ microservice-projects/eureka-demo/zuul-gateway/src/test/java/com/example/zuulgateway/ZuulGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.abhiram.zuul.example;
+package com.example.zuulgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZuulExampleApplicationTests {
+class ZuulGatewayApplicationTests {
 
     @Test
     void contextLoads() {
